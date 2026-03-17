0784a2b84d4987ddc7cd2ddf608b62cd8dec576a
Marco Hernandez <mhernandezt23@miumg.edu.gt>
2026-03-16 18:40:48 -0600

    Subiendo proyectos

---
+++ practicaBaseDeDatosBuena/src/main/java/com/mycompany/practicabasededatosbuena/PracticaBaseDeDatosBuena.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicabasededatosbuena;
+
+/**
+ *
+ * @author macoh
+ */
+public class PracticaBaseDeDatosBuena {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
